f83efe1f2a361c95fc08b72265f5f286e47deda4
SangGeun.Jeong <119453895+toproot@users.noreply.github.com>
2025-04-07 20:51:23 +0900

    Title: 달에서 무게 구하기, Time: 97ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250407/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 13;
+        float b = 0.165f;
+        float c = a*b;
+
+        System.out.printf(a + " * %.6f = %.6f", b,c);
+    }
+}
