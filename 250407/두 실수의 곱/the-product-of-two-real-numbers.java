316a0545cc0a4d8654c5a4dfa080de04567ac6a3
SangGeun.Jeong <119453895+toproot@users.noreply.github.com>
2025-04-07 20:58:21 +0900

    Title: 두 실수의 곱, Time: 98ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250407/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        float a = 5.26f;
+        float b = 8.27f;
+
+        float c = a*b;
+
+
+        System.out.printf("%.3f", c);
+    }
+}
