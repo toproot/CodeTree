cfe883adebde59b44da10d7cca6b086578eca54e
SangGeun.Jeong <119453895+toproot@users.noreply.github.com>
2025-04-07 21:15:49 +0900

    Title: 변수 값 교체하기 3, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250407/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 3;
+        int b = 5;
+        int temp = 0;
+
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
