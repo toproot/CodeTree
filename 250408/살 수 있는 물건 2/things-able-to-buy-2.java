34e484baa117a5b1e1544cc7f2d8027457bf9034
SangGeun.Jeong <119453895+toproot@users.noreply.github.com>
2025-04-08 19:47:27 +0900

    Title: 살 수 있는 물건 2, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250408/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        String product = "";
+
+        if (N >= 3000) {
+            product = "book";
+        }else if (N >= 1000) {
+            product = "mask";
+        }else if (N >= 500) {
+            product = "pen";
+        }else {
+            product = "no";
+        }
+
+        System.out.print(product);
+
+    }
+}
