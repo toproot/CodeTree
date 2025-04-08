252a666e8ae9cf2cb8cddbcbf5786ba150c4e814
SangGeun.Jeong <119453895+toproot@users.noreply.github.com>
2025-04-08 22:38:35 +0900

    Title: 특정 숫자 도달하기 2, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250408/특정 숫자 도달하기 2/reaching-specific-number-2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i =N; i>=1; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
