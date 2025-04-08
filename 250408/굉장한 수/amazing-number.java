0e33f8d4f04cf98e4e6d6deecb4e5dee61a2cf02
SangGeun.Jeong <119453895+toproot@users.noreply.github.com>
2025-04-08 21:25:46 +0900

    Title: 굉장한 수, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250408/굉장한 수/amazing-number.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        if ((N%2 != 0 && N%3 ==0) || (N%2 == 0 && N%5 ==0)){
+            System.out.print("true");
+        }else {
+            System.out.print("false");
+        }
+    }
+}
