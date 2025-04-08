c69825053c37b56b311bd04b15b1a4943252c663
SangGeun.Jeong <119453895+toproot@users.noreply.github.com>
2025-04-08 20:48:17 +0900

    Title: 특정 조건 두 정수 비교, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250408/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        if (A < B) {
+            System.out.print(1);    
+        }else {
+            System.out.print(0);
+        }
+
+        if (A == B) {
+            System.out.print(" "+ 1);
+        }else {
+            System.out.print(" " + 0);
+        }
+    }
+}
