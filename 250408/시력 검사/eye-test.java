d5010af2fabb7c609e2fe9fa74b47f123cdf7132
SangGeun.Jeong <119453895+toproot@users.noreply.github.com>
2025-04-08 21:06:27 +0900

    Title: 시력 검사, Time: 133ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250408/시력 검사/eye-test.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        if (a >= 1.0 && b >= 1.0) {
+            System.out.print("High");
+        }else if (a >= 0.5 && b >= 0.5) {
+            System.out.print("Middle");
+        }else {
+            System.out.print("Low");
+        }
+
+    }
+}
