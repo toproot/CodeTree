ecddb83792b230f2ce40b5f5a0e0586b2199ab20
SangGeun.Jeong <119453895+toproot@users.noreply.github.com>
2025-04-14 20:36:44 +0900

    Title: 숫자 삼각형, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

--- 250414/숫자 삼각형/number-triangle.java
+++ 250414/숫자 삼각형/number-triangle.java
@@ -6,7 +6,7 @@ public class Main {
 
         int N = sc.nextInt();
 
-        for(int i = 1; i <=N; i++) {
+        for(int i = 1; i <= N; i++) {
             for(int j = 1; j < i+1; j++) {
                 System.out.print(j + " ");
             }
