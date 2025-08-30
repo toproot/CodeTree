023c49c89bdbc3e17e0dc965b3bc2e33ffbae24f
SangGeun.Jeong <119453895+toproot@users.noreply.github.com>
2025-08-30 15:58:44 +0900

    Title: 좌우 지그재그 출력, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250830/좌우 지그재그 출력/print-lr-zigzag.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int iFlagNo = 1;
+
+        for(int i = 1; i < N+1; i++) {
+            for(int j = 0; j < N; j++) {
+                if (j == 0) {
+                    System.out.print(iFlagNo + " ");
+                }else if (i % 2 == 1) {
+                    iFlagNo++;
+                    System.out.print(iFlagNo + " ");
+                }else {
+                    iFlagNo--;
+                    System.out.print(iFlagNo + " ");
+                }
+            }
+            iFlagNo += 4;
+            System.out.println();
+        }
+    }
+}
