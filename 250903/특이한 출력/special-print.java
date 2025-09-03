3f016a81fdd2b112071e86776a89027598324683
SangGeun.Jeong <119453895+toproot@users.noreply.github.com>
2025-09-03 22:04:23 +0900

    Title: 특이한 출력, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250903/특이한 출력/special-print.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for(int i = 1; i < N+1; i++) {
+            for(int j = 1; j < N+1; j++) {
+                System.out.print("("+ i + ", " + j + ") ");
+
+                if ((i+j) % 4 == 0) {
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
