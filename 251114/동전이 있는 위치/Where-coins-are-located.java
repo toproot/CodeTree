25913daa5ce84d541c9d74dc344b8e2f7bffc5fc
SangGeun.Jeong <119453895+toproot@users.noreply.github.com>
2025-11-14 21:33:28 +0900

    Title: 동전이 있는 위치, Time: 135ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251114/동전이 있는 위치/Where-coins-are-located.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[][] iList = new int[N+1][N+1];
+
+        for(int i = 0; i < M; i++) {
+            int r = sc.nextInt();
+            int c = sc.nextInt();
+
+            iList[r-1][c-1] = 1;
+        }
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                System.out.print(iList[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
