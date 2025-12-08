9607de7ae714daebe6d4f357fd64f08946a979ce
SangGeun.Jeong <119453895+toproot@users.noreply.github.com>
2025-12-08 20:10:32 +0900

    Title: 소문자와 숫자, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251208/소문자와 숫자/letter-and-number.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        for(int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (c >= 'A' && c <= 'Z') {
+                int n = 'a' + (int)(c - 'A');
+                System.out.print((char)n);
+            }else if(c >= 'a' && c <= 'z'){
+                System.out.print(c);
+            }else if (c >= '1' && c <= '9') {
+                System.out.print(c);
+            }
+        }
+    }
+}
