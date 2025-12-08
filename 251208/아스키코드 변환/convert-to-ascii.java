9334f27d968a3b7461109dabd962ef7b21162690
SangGeun.Jeong <119453895+toproot@users.noreply.github.com>
2025-12-08 19:35:43 +0900

    Title: 아스키코드 변환, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251208/아스키코드 변환/convert-to-ascii.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        char c = sc.next().charAt(0);
+        int n = sc.nextInt();
+
+        System.out.print((int)c);
+        System.out.print(" ");
+        System.out.print((char)n);
+    }
+}
