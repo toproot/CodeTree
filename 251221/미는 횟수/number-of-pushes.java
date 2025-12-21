93847c43ea190d11e30cf9e9346c5e8758e246c7
SangGeun.Jeong <119453895+toproot@users.noreply.github.com>
2025-12-21 18:04:13 +0900

    Title: 미는 횟수, Time: 117ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251221/미는 횟수/number-of-pushes.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        String A = sc.next();
+        String B = sc.next();
+        int cnt = 0;
+        boolean bResult = false;
+
+        for (int i = 0 ; i < A.length(); i++) {
+            cnt++;
+            String subA = A.substring(1) + A.substring(0, 1);
+            if (subA.equals(B)) {
+                System.out.print(cnt);
+                bResult = true;
+                break;
+            }else {
+                A = subA;
+            }
+        }
+        if(!bResult) {
+            System.out.print("-1");            
+        }
+    }
+}
