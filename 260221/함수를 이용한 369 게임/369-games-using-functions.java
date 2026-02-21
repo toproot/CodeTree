1b9375f0b60c56563b1701ae4292cc998f2c9bb6
SangGeun.Jeong <119453895+toproot@users.noreply.github.com>
2026-02-21 21:43:34 +0900

    Title: 함수를 이용한 369 게임, Time: 186ms, Memory: 19MB, Status: Wrong Answer - Codetree

---
+++ 260221/함수를 이용한 369 게임/369-games-using-functions.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        // Please write your code here.
+
+        int cnt = 0;
+
+        for(int i = A; i <= B; i++) {
+            if(isMagicNumber(i)) {
+                cnt++;
+            }
+        }
+
+        System.out.print(cnt);
+    }
+
+    public static boolean isMagicNumber(int i) {
+        boolean bReturn = false;
+
+        if((i % 3 == 0) || isSpecialNumber(i)) {
+            bReturn = true;
+        }
+
+        return bReturn;
+    }
+
+    public static boolean isSpecialNumber(int i) {
+        boolean bReturn = false;
+        while(i > 0) {
+
+            if (i / 10 == 0) {
+                i = i % 10;
+                if(isThreeNumber(i)) {
+                    bReturn = true;
+                }
+                break;
+            }else {
+                int j = i / 10;
+                if(isThreeNumber(j)) {
+                    bReturn = true;
+                    break;
+                }
+            }
+
+            i = i % 10;
+        }
+
+        return bReturn;
+    }
+
+    public static boolean isThreeNumber(int j) {
+        boolean bReturn = false;
+        int[] iList = new int[]{3, 6, 9};
+        for(int i = 0; i < iList.length; i++) {
+            if (j == iList[i]) {
+                bReturn = true;
+                break;
+            }
+        }
+
+        return bReturn;
+    }
+}
