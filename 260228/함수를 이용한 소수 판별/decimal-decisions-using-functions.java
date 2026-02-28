3946c2069f8a706ebd940898ccb3208d12f332b5
SangGeun.Jeong <119453895+toproot@users.noreply.github.com>
2026-02-28 22:58:28 +0900

    Title: 함수를 이용한 소수 판별, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260228/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // Please write your code here.
+
+        int sum = 0;
+
+        for(int i = a; i <= b; i++) {
+            if(isPrime(i)) {
+                sum += i;
+            }
+        }
+
+        System.out.print(sum);
+    }
+
+    public static boolean isPrime(int n) {
+        for(int i = 2; i < n; i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
